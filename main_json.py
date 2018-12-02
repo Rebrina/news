aa4236fbd71f8af7192a61b5b0f9b9d18cef3c55
Rebrina <feurioster@gmail.com>
2018-12-03 01:32:35 +0300

    1

--- main_json.py
+++ main_json.py
@@ -7,4 +7,28 @@ import json
 
 with open("files/newsafr.json", encoding="utf_8_sig") as datafile:
   js_data = json.load(datafile)
-print(js_data)
+
+news_content = {}
+news_description =''
+i = 0
+
+for rss in js_data:
+  for channel in js_data:
+    for items in js_data:
+      while i < len(js_data['rss']['channel']['items']):
+        news_content = js_data['rss']['channel']['items'][i]['description']
+        news_description = news_description + news_content
+        i += 1
+        print(i)
+
+a = news_description.split(' ')
+
+print(a)
+
+def sortByLength(inputStr):
+        return len(inputStr)
+
+a.sort(key=sortByLength, reverse=True)
+
+for i in range(0,10):
+  print(a[i])
